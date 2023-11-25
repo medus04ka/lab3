59080fb16beafe636f2e1ea1810852004fc044f3
medus04ka <ayebratve2289797@gmail.com>
2023-11-26 00:57:45 +0300

    1234

--- src/Santik.java
+++ src/Santik.java
@@ -3,7 +3,7 @@ import java.util.Random;
 class Santik implements ShelfItem, SantikInteraction {
     @Override
     public void use() {
-        System.out.println("Козлик уронил сантик.");
+        System.out.println("Козлик посмотрел на цену, указанную на механическом язычке.");
     }
 
     @Override
